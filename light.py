352a72f122885ec1856e9009b93d66192cf6afd2
helkaweth <joe.lavoie@colupon.com>
2017-05-03 08:34:20 -0400

    Update light.py

--- light.py
+++ light.py
@@ -6,7 +6,7 @@ def turnoff():
 	gc.enable()
 	threading.Timer(5.0,turnoff).start()
 	payload = '{"on": false}'
-	r = requests.put("http://192.168.1.110/api/4AwLcVXAZJFY0mGbijjhgHG95PS7J4ROAheG1LO2/lights/8/state",data=payload)
+	r = requests.put("http://x.x.x.x/api/x/lights/8/state",data=payload)
 	gc.collect()
 	gc.disable()
 turnoff()
